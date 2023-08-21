f9b8181965977ff83b3b695f19c225a449f2cf5a
siddhesh <siddheshangane142000@gmail.com>
2023-08-22 00:10:31 +0530

    add docker file

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from typing import Union
 
 from fastapi import FastAPI
+from .settings import Settings
 
 app = FastAPI()
+setting = Settings()
 
 
 @app.get("/")
---
+++ settings.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # app_name: str = "Awesome API"
+    # admin_email: str
+    # items_per_user: int = 50
+    BROKER_ADDRESS = "your_broker_ip"
+    BROKER_PORT = 1883
