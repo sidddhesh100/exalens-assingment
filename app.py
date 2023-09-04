231feda512941d77b7b67c0f2b2bd50ed42c2c09
Siddhesh Angane <siddheshangane142000@gmail.com>
2023-09-05 00:17:04 +0530

    getting redis issue

--- app.py
+++ app.py
@@ -19,17 +19,17 @@ app = FastAPI()
 # mqtt_client = mqtt.Client()
 # mqtt_client.connect(os.getenv('BROKER_ADDRESS'), os.getenv('BROKER_PORT'))
 
-mqtt_config = MQTTConfig(username=os.getenv("USERNAME"), password=os.getenv("PASSWORD"))
+mqtt_config = MQTTConfig(username="root", password="example")
 
 mqtt = FastMQTT(config=mqtt_config)
 
 mqtt.init_app(app)
 
-
-mongo_client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGO_DB_URL"))
+# import pdb; pdb.set_trace()
+mongo_client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://root:example@localhost:27017")
 database = mongo_client.exalon
 
-redis = Redis(host="localhost", port=os.getenv("REDIS_PORT"), db=0)
+redis = Redis(host="localhost", port=6379, db=0)
 
 
 @app.get("/")
@@ -45,22 +45,23 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/mimic_readings")
 def mimic_readings():
     sensor_ids = ["sensor1", "sensor2", "sensor3"]
-    try:
-        i = 0
-        while True:
-            for sensor_id in sensor_ids:
-                temperature_topic = f"sensors/temperature/{sensor_id}"
-                humidity_topic = f"sensors/humidity/{sensor_id}"
+    # try:
+    #     i = 0
+    #     while True:
+    for sensor_id in sensor_ids[:1]:
+        temperature_topic = f"sensors/temperature/{sensor_id}"
+        humidity_topic = f"sensors/humidity/{sensor_id}"
 
-                publish_sensor_reading(sensor_id, temperature_topic)
-                publish_sensor_reading(sensor_id, humidity_topic)
+        publish_sensor_reading(sensor_id, temperature_topic)
+        # publish_sensor_reading(sensor_id, humidity_topic)
 
-                time.sleep(5)  # Wait for 5 seconds before publishing again
-            print()
-            print(i)
-    except KeyboardInterrupt:
-        print("Client stopped")
-    return Response(content={"status": True}, media_type="application/json")
+        time.sleep(5)  # Wait for 5 seconds before publishing again
+    #         print()
+    #         print(i)
+    # except KeyboardInterrupt:
+    #     print("Client stopped")
+    # import pdb; pdb.set_trace()
+    return Response(content=json.dumps({"status": True}), media_type="application/json")
 
 
 def publish_sensor_reading(sensor_id, topic):
@@ -91,12 +92,30 @@ async def message(client, topic, payload, qos, properties):
     return 0
 
 
-@mqtt.subscribe("my/mqtt/topic/#")
-async def message_to_topic(client, topic, payload, qos, properties):
-    print(
-        "Received message to specific topic: ", topic, payload.decode(), qos, properties
-    )
+@mqtt.subscribe("sensors/temperature/#")
+async def temperature_to_topic(client, topic, payload, qos, properties):
+    print("Received message to specific topic: ", topic, payload.decode(), qos, properties)
+    payload = json.loads(payload.decode())
     database["sensor_reading"].insert_one(payload)
+    sensor_id = payload["sensor_id"]
+    redis_key = f"temperature-{sensor_id}"
+    import pdb
+
+    pdb.set_trace()
+    # redis.set(redis_key, [])
+    redis.lpush(redis_key, payload)
+    redis.ltrim(redis_key, 0, 9)
+
+
+@mqtt.subscribe("sensors/humidity/#")
+async def humidity_to_topic(client, topic, payload, qos, properties):
+    print("Received message to specific topic: ", topic, payload.decode(), qos, properties)
+    payload = json.loads(payload.decode())
+    database["sensor_reading"].insert_one(payload)
+    sensor_id = payload["sensor_id"]
+    redis_key = f"humidity-{sensor_id}"
+    redis.lpush(redis_key, payload)
+    redis.ltrim(redis_key, 0, 9)
 
 
 @mqtt.on_disconnect()
